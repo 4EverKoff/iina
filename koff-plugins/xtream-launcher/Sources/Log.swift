0fbe77a409ee26104b725a67ef90ee88a85aa256
Koff <koffi.dorkenoo@gmail.com>
2026-08-09 17:27:20 +0200

    Retire xtream-launcher : code déplacé vers le repo dédié XTREAM-LAUNCHER
    Le lanceur devient un projet autonome (repo privé 4EverKoff/XTREAM-LAUNCHER).
    Le plugin xtream-player reste inchangé.

--- koff-plugins/xtream-launcher/Sources/Log.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Log sanitizé (compteurs/codes d'erreur uniquement) + flush immédiat
-/// pour rester visible quand stdout est redirigé vers un fichier.
-func xlog(_ message: String) {
-    print(message)
-    fflush(nil)
-}
